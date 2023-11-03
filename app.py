67b2b9c3b8f11651a76bf6274381e79ec409d8db
Ramiz Ur Rahman Khan <84407885+ramizkhan06@users.noreply.github.com>
2023-11-03 12:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ from models import *
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+     app.run(debug=True)
